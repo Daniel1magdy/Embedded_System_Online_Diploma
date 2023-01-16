633985d40b37ca70e00e860213755afd0e5e527e
Daniel1magdy <93931368+Daniel1magdy@users.noreply.github.com>
2023-01-16 17:16:58 +0200

    Move Some Projects.

---
+++ Unit 2/Lec_lab/leclab/leclab.c
@@ -0,0 +1,26 @@
+/*
+ * leclab.c
+ *
+ *  Created on: Jan 15, 2023
+ *      Author: DaNil
+ */
+
+#include <stdio.h>
+
+void main (void)
+{
+	int i=0,j=0 ,h=0;
+	for (i = 9; i>=0; i-- )
+	{
+		for (j = h; j<=9; j++ )
+			{
+				printf("%d ",j);
+
+			}
+		printf("\n");
+		h++;
+
+	}
+
+
+}
